68df5b45bdb16091f570e7634e8c2debd84a8405
Ashutosh Anand <ashutoshanand120704@gmail.com>
2026-05-07 15:17:15 +0530

    [Ashutosh] Add: Python script to return sum of two numbers removed hard-coded values for total_sum and bonus_points

--- program5-2.py
+++ program5-2.py
@@ -4,8 +4,8 @@ def calculate_sum(x: int, y: int):
     """
     return x + y
 # 1.Describing variables and 2. Using f-strings to print the result
-total_sum = int(input("Enter value for total_sum "))
-bonus_points = int(input("Enter value for bonus_points "))
+total_sum = input("Enter value for total_sum ")
+bonus_points = input("Enter value for bonus_points ")
 print(f"Data type of total_sum is {type(total_sum)} and bonus_points is {type(bonus_points)}")
 final_score = calculate_sum(total_sum, bonus_points)
 print(f"Final score is {final_score}")
