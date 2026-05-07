f37cef8535a25731a97974837e376726925e2367
Ashutosh Anand <ashutoshanand120704@gmail.com>
2026-05-07 15:31:10 +0530

    [Ashutosh] Add: Adding the error handling to identify value errors

---
+++ program6-2.py
@@ -0,0 +1,16 @@
+def calculate_sum(x: int, y: int):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return x + y
+# 1.Describing variables and 2. Using f-strings to print the result
+try:
+    total_sum = int(input("Enter value for total_sum "))
+    bonus_points = int(input("Enter value for bonus_points "))
+    print(f"Data type of total_sum is {type(total_sum)} and bonus_points is {type(bonus_points)}")
+    final_score = calculate_sum(total_sum, bonus_points)
+    print(f"Final score is {final_score}")
+except ValueError as err:
+    print("Oops! Got a {err} error. Try again and Enter a proper value.")
+except:
+    print("Try again and Enter a proper value.")
