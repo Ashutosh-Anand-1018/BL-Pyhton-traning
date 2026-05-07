90aff2ff9fd1f509df08ba82355c8ac72d5c5e00
Ashutosh Anand <ashutoshanand120704@gmail.com>
2026-05-07 15:04:44 +0530

    [Ashutosh] Add: Adding comments to function and its usage

---
+++ functions.py
@@ -0,0 +1,10 @@
+def calculate_sum(x,y):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return x + y
+# 1.Describing variables and 2. Using f-strings to print the result
+total_sum = 10
+bonus_points = 20
+final_score = calculate_sum(total_sum, bonus_points)
+print(f"Final score is {final_score}") 
