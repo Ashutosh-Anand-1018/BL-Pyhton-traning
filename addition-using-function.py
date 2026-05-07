13968a555540ef246efaafbca14f659214fe9020
Ashutosh Anand <ashutoshanand120704@gmail.com>
2026-05-07 15:03:47 +0530

    [Ashutosh] Add: Script for addition using functions

---
+++ addition-using-function.py
@@ -0,0 +1,11 @@
+x = 10
+y = 20
+z = x + y
+print(f"The value z is {z}")
+
+def add(x,y):
+    return x + y
+total_sum = 10
+bonus_points = 20
+final_score = add(total_sum, bonus_points)
+print(f"The final score is {final_score}")
