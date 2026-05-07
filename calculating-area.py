e31c15b3d91211b7ad93e0db5b6e6cb5d6d7816f
Ashutosh Anand <ashutoshanand120704@gmail.com>
2026-05-07 15:10:54 +0530

    [Ashutosh] Add: Python script to calculate area of circle with hard-coded values and constant

---
+++ calculating-area.py
@@ -0,0 +1,7 @@
+def calculate_area(radius):
+    return 3.14 * radius * radius
+pi = 3.14
+def calculate_area(radius):
+    return pi * radius * radius
+radius = 2
+print(f"The area of the circle with radius {radius} is {calculate_area(radius)}")
